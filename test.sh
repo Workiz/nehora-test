2a94dd14591d458963e0a83da07384f2689add96
nehora <nehora.moshe@workiz.com>
2022-07-25 11:33:41 +0300

    test

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "out"
-if [[ "[DEVOPS-369]" =~ \[(.*)\](.*) ]]; then
-    echo "here"
-    src=${BASH_REMATCH[1]}
-    echo $src
-fi
+# echo "out"
+# if [[ "[DEVOPS-369]" =~ \[(.*)\](.*) ]]; then
+#     echo "here"
+#     src=${BASH_REMATCH[1]}
+#     echo $src
+# fi
+
+if [[ "${{github.head_ref}}" =~ ([A-Z]+-[1-9]+).* ]]; then
+            BRANCH_TITEL=${BASH_REMATCH[1]}
+            echo $BRANCH_TITEL
+          fi
