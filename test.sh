8bd02e545a78b528733d8378401e29e07d34871c
nehora <nehora.moshe@workiz.com>
2022-08-15 10:32:34 +0300

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ -z "[{'name' :' Allure Report','link': 'https://storage.googleapis.com/workiz-qa-reports/allure-report--/index.html'}, {'name': ' Link to GitHub Action' , 'link' : 'https://github.com/Workiz/native-automation/actions/runs/2859087204'}]" ]]; then
+if [[ -z '"[{"name" :" Allure Report","link": "https://storage.googleapis.com/workiz-qa-reports/allure-report-${{env.PLATFORM}}-${{env.APPHASH}}/index.html"},
+                      {"name": " Link to GitHub Action" , "link" : "https://github.com/Workiz/native-automation/actions/runs/${{github.run_id}}"}]"' ]]; then
   echo "hi"
 else
   echo "here"
