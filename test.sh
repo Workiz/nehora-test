39be6918fc9c76c7fa14c7722aeceb4bf2881418
nehora <nehora.moshe@workiz.com>
2022-08-22 09:56:41 +0300

    delete-branches-script

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
- str="this is a string"
-
-if [[ ${#str} > 1 ]]; then
-  echo "hi"
-else
-  echo "here"
+exists=`git show-ref refs/heads/rc3gf.336-branch`
+if [ -n "$exists" ]; then
+    echo 'branch exists!'
 fi
-
-n=${#str}
-echo "Length of the string is : $n "
