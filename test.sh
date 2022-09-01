d3cb6be430053b30fbc428f0566595befefa349f
nehora <nehora.moshe@workiz.com>
2022-09-01 17:25:35 +0300

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-exists=`git show-ref refs/heads/rc3gf.336-branch`
-if [ -n "$exists" ]; then
-    echo 'branch exists!'
+# exists=`git show-ref refs/heads/rc3gf.336-branch`
+# if [ -n "$exists" ]; then
+#     echo 'branch exists!'
+# fi
+
+status=$(curl --location --request POST 'https://infra-api.workiz.com/releaseProgress/get_train_status/')
+if [[ "$status" == "false" ]]; then
+    echo "Error - train is lock now"
+    exit 1
 fi
