2c8b63d199b58f41a52363ab5d9845b213bfee84
nehora <nehora.moshe@workiz.com>
2023-04-02 12:59:42 +0300

    test

--- test.sh
+++ test.sh
@@ -16,6 +16,7 @@ branch="123-branch"
 # -H "Authorization: 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" \
 # https://api.github.com/repos/Workiz/nehora-test/dispatches \
 # -d '{"event_type":"train","client_payload":{"tag":"9380","branch": "dnsk" }}'
+
 curl -X POST -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer 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" \
         https://api.github.com/repos/Workiz/nehora-test/dispatches \
